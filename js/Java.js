4b3880d01cb04134304a63a04ffdf3671b6b754a
Mucco2 <161291343+Mucco2@users.noreply.github.com>
2025-05-15 19:18:16 +0200

    great comit

---
+++ js/Java.js
@@ -0,0 +1,30 @@
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    let top = window.scrollY;
+
+    sections.forEach(sec => {
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+            });
+
+            let currentLink = document.querySelector(`header nav a[href="#${id}"]`);
+            if (currentLink) {
+                currentLink.classList.add('active');
+            }
+        }
+    });
+
+    // sticky navbar
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    
+};
