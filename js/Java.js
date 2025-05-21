0d95504199e9277aa93bfabead4fbfc681136556
Mucco2 <161291343+Mucco2@users.noreply.github.com>
2025-05-21 17:30:55 +0200

    comit

--- js/Java.js
+++ js/Java.js
@@ -53,5 +53,7 @@ ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .service-container, .portfolio-box, .contact form', {origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+ScrollReveal().reveal('.home-img, .MuccosPhoto.png', { origin: 'top' });
+
 
 
